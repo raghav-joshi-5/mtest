b3ad942f83b82401b716bd1f61607c65b65281d6
raghav-joshi-5 <raghav.joshi2017@gmail.com>
2024-08-06 13:26:30 +0530

    tmdb_foreach_1

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+let cl=console.log;
+
+const moviecardContainer=document.getElementById('moviecardContainer');
+
+let moviecardhtml='';
+
+let setreating=(rating)=>{
+	if(rating >7.5){
+		return 'bg-success'
+	}else if(rating <= 7.5 && rating >5){
+		return 'bg-warning'
+	}else{
+		return 'bg-danger'
+	}
+}
+
+
+movieArray.forEach((movie)=>{
+	moviecardhtml +=`
+					<div class="col-4 mb-4">
+						<div class="card">
+							<figure class="movieCard">
+								<img 
+									src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" 
+									alt="moviePoster" 
+									title="moviePoster"/>
+								<figcaption>
+									<div class="movieTitle">
+										<div class="row">
+											<div class="col-10">
+												<h3 class="m-0">
+													${movie.title}
+												</h3>
+											</div>
+											<div class="col-2 align-self-center">
+												<span class="${setreating(movie.vote_average)} rating">${movie.vote_average}</span>
+											</div>
+										</div>
+									</div>
+									<div class="movieOverview">
+										<h3>${movie.title}</h3>
+										<p class="m-0">
+											<em>overview</em>
+											${movie.overview}
+										</p>
+									</div>
+								</figcaption>
+							</figure>
+						</div>
+					</div>
+					`
+})
+
+moviecardContainer.innerHTML=moviecardhtml;
